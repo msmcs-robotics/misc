a76e6387df61177199b1305c5a6828f97b783b8f
msc_intra <109829955+mscrobotics@users.noreply.github.com>
2022-09-28 15:15:36 -0800

    launcher of things

---
+++ catapult/catapult.ino
@@ -0,0 +1,27 @@
+#include <HCSR04.h>
+
+int ledPin = 13; //Relay Pin
+int base_delay = 1000;
+
+int us1T = 5; //Trigger Pn 
+int us1E = 6; //Echo Pin
+
+int min_range = 10.00;
+
+HCSR04 us1(us1T, us1E); //initialisation class HCSR04 (trig pin , echo pin)
+
+void setup() { 
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    if (us1.dist() <= min_range && us1.dist() != 0.00) {
+      digitalWrite(ledPin, HIGH);
+      delay(base_delay);
+      digitalWrite(ledPin, LOW);
+    }
+    Serial.println(us1.dist());
+    
+}
